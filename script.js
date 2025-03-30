a2f6d5980c4e9ca27e3805c75e4560050094a6ef
SpotLanguage <alexmca1399@gmail.com>
2025-03-30 14:03:59 +0200

    Adicionar arquivos do site

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Função para mostrar o idioma selecionado
+document.getElementById("langSelect").addEventListener("change", function () {
+  alert("Idioma selecionado: " + this.value);
+});
+
+// Função para mostrar a moeda selecionada
+document.getElementById("currencySelect").addEventListener("change", function () {
+  alert("Moeda selecionada: " + this.value);
+});
